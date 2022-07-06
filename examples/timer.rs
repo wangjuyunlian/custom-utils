55159dbfc1610dc5ba7cf85d0db11d70b5f046fe
jmhuang <jm.observer@qq.com>
2022-07-06 23:31:47 +0800

    更新timer-util

--- examples/timer.rs
+++ examples/timer.rs
@@ -6,7 +6,7 @@ use time::OffsetDateTime;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     logger_stdout_debug();
-    let conf = DayHourMinuterSecondConf::default_week_days(WeekDays::default_value(W6))
+    let conf = configure_weekday(WeekDays::default_value(W6))
         .build_with_hours(Hours::default_all())
         .build_with_minuter(Minuters::default_array(&[M0, M10, M20, M30, M40, M50]))
         .build_with_second(Seconds::default_array(&[S0, S30]));
@@ -15,7 +15,7 @@ async fn main() -> anyhow::Result<()> {
 
     let handle = tokio::spawn(async move {
         loop {
-            let off_seconds = conf.next().unwrap();
+            let off_seconds = conf.next();
             println!("next seconds: {}", off_seconds);
             tokio::time::sleep(Duration::from_secs(off_seconds)).await;
             println!("{:?}", OffsetDateTime::now_local().unwrap());
